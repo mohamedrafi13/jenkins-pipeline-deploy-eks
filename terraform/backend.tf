d9d9894d7e11b02cbe6b4ba703e36c11eb05c275
developer1 <rafi.huta@gmail.com>
2024-01-03 12:19:23 +0530

    updated the s3 backend file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
+    bucket = "thirafi.live"
+    region = "ap-south-1"
     key = "eks/terraform.tfstate"
   }
-}
+} 
